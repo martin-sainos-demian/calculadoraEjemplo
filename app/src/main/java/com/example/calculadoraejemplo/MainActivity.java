d82ddd05efe27f9392cb9775f29dc273f21762ef
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-02-16 20:35:52 -0600

    more 2

--- app/src/main/java/com/example/calculadoraejemplo/MainActivity.java
+++ app/src/main/java/com/example/calculadoraejemplo/MainActivity.java
@@ -189,6 +189,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         UpdateRes();
+        newNum = true;
     }
 
     public void Inverse(View view) {
